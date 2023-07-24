2ad639a1db29a03b0b6d84e2b4992127ba21d729
Jesse Ilc <jesseilc123@gmail.com>
2023-07-24 13:33:37 -0500

    Tailwind installed for front end, ready to use. Working on server next to make sure the back end is working properly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+  return (
+    <div className="text-sky-400">Hello World</div>
+  )
 }
 
 export default App;
