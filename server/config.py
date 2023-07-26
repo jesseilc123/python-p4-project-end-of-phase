f5789c834c3ad6583ca0cad7999d8a704963ce40
Jesse Ilc <jesseilc123@gmail.com>
2023-07-26 12:06:44 -0500

    added debug.py file, will test upgrade tables and test to ensure they are working correctly

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaDatar
+from sqlalchemy import MetaData
 
 # Local imports
 
--- server/models.py
+++ server/models.py
@@ -11,24 +11,25 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
 
     username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String)
+    password = db.Column(db.String)
+    # _password_hash = db.Column(db.String)
 
     posts =  db.relationship("Post", backref="user")
     comments = db.relationship("Comment", backref="user")
 
-    @hybrid_property
-    def password_hash(self):
-        raise AttributeError
+    # @hybrid_property
+    # def password_hash(self):
+    #     raise AttributeError
 
-    @password_hash.setter
-    def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+    # @password_hash.setter
+    # def password_hash(self, password):
+    #     password_hash = bcrypt.generate_password_hash(
+    #         password.encode('utf-8'))
+    #     self._password_hash = password_hash.decode('utf-8')
 
-    def authenticate(self, password):
-        return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+    # def authenticate(self, password):
+    #     return bcrypt.check_password_hash(
+    #         self._password_hash, password.encode('utf-8'))
 
 class Post(db.Model, SerializerMixin):
     __tablename__ = 'posts'
